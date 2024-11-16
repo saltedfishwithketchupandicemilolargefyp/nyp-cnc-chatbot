51ea14fefbb83caf5361cf79dfe10e74dd4fe802
skyqii <140032908+skyqii@users.noreply.github.com>
2024-11-17 00:17:32 +0800

    adding evaluations

--- modelling/modelEval.py
+++ modelling/modelEval.py
@@ -1,149 +1,24 @@
-# MODEL WITH CONVO HIST AND MULTI QUERY CAPABILITY
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_chroma import Chroma
-import openai
-import os
-from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, StateGraph
-from langgraph.graph.message import add_messages
-from typing_extensions import Annotated, TypedDict
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from dotenv import load_dotenv
-from typing import Sequence
-from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.prompts import PromptTemplate
+from langsmith import Client
 
-LANGCHAIN_TRACING_V2="true"
-LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
-LANGCHAIN_API_KEY= os.getenv("LANGCHAIN_API_KEY")
-LANGCHAIN_PROJECT="fypj-aichatbot"
+# dataset creation (can be done on langsmith web)
+dataset_name = "RAG Chatbot Dataset"
 
-# Load environment variables from .env file
-load_dotenv()
-# Access the variables from the .env file
-CHROMA_PATH = os.getenv("CHROMA_PATH")
-DATA_PATH = os.getenv("DATA_PATH")
-openai.api_key = os.getenv("OPENAI_API_KEY")
-EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+client = Client()
 
-# Set the LLM with streaming enabled
-llm = ChatOpenAI(temperature=0.8, model="gpt-4o-mini")
+dataset = client.create_dataset(dataset_name, description="A small dataset containing QA from RAG chatbot")
 
-# Load embeddings and Chroma database
-embedding = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding)
-
-# Define the multi-query generation template for retriever
-multi_query_template = PromptTemplate(
-    template=(
-        "The user has asked a question: {question}.\n"
-        "1. First, check if it is a complex question or multiple related questions. "
-        "If yes, split the query into distinct questions if there are multiple and move on to point 2."
-        "Else, just return the question, and break.\n"
-        "2. Then, for each distinct question, generate 3 rephrasings that would return "
-        "similar but slightly different relevant results.\n"
-        "Return each question on a new line with its rephrasings.\n"
-    ),
-    input_variables=["question"],
+# Filter runs to add to the dataset
+runs = client.list_runs(
+    project_name="fypj-ai-chatbot",
+    execution_order=1,
+    error=False,
 )
 
-# Create the multi-query retriever
-multiquery_retriever = MultiQueryRetriever.from_llm(
-    retriever=db.as_retriever(search_kwargs={'k': 3}),
-    llm=llm,
-    prompt=multi_query_template
-)
+for run in runs:
+    client.create_example(
+        inputs=run.inputs,
+        outputs=run.outputs,
+        dataset_id=dataset.id,
+    )
 
-# Prompt to get the chat history context for follow-up questions
-contextualize_q_system_prompt = (
-    "Given a chat history and the latest user question "
-    "which might reference context in the chat history, "
-    "formulate a standalone question which can be understood "
-    "without the chat history. Do NOT answer the question, "
-    "just reformulate it if needed and otherwise return it as is."
-)
-
-# Setting the prompt template for history context
-contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-
-# Create history-aware retriever with multi-query integration
-history_aware_retriever = create_history_aware_retriever(
-    llm, multiquery_retriever, contextualize_q_prompt
-)
-
-# System prompt for question answering
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use ONLY the following pieces of retrieved context to answer the questions. "
-    "Answer the following question and avoid giving any harmful, inappropriate, or biased content. "
-    "Respond respectfully and ethically. Do not answer inappropriate or harmful questions. "
-    "If the answer does not exist in the vector database, "
-    "nicely inform the user that you cannot answer questions that are not in the NYP CNC database. "
-    "Keep the answer concise."
-    "\n\n"
-    "{context}"
-)
-
-# Creating the prompt template for question answering
-qa_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-
-# Create the question-answer chain with multi-query retriever
-question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
-
-# Statefully manage chat history
-class State(TypedDict):
-    input: str
-    chat_history: Annotated[Sequence[BaseMessage], add_messages]
-    context: str
-    answer: str
-
-# Define the function to call the model and manage state with streaming
-def call_model(state: State):
-    response = rag_chain.invoke(state)
-    return {
-            "chat_history": [
-                HumanMessage(state["input"]),
-                AIMessage(response["answer"]),
-            ],
-            "context": response["context"],
-            "answer": response["answer"],
-        }
-
-# Create the workflow with the state graph
-workflow = StateGraph(state_schema=State)
-workflow.add_edge(START, "model")
-workflow.add_node("model", call_model)
-
-# Compile the graph with a checkpointer to persist state
-memory = MemorySaver()
-app = workflow.compile(checkpointer=memory)
-
-config = {"configurable": {"thread_id": "abc123"}}
-
-# Main interaction loop
-print("Enter your question here ('Exit' to end):")
-while True:
-    question = input()
-    if question.lower() == "exit":
-        break
-    response = app.invoke({"input": question}, config=config)
-    print(response["answer"])
-
-    print()  
-    print("Enter your question here ('Exit' to end):")
+# running the evaluation on the dataset
